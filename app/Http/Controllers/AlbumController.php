562ef75920b059e54ca63cf6c3d8fab96e5898c4
Nicolas Chevalier <nicolas.mailip@gmail.com>
2022-07-18 19:42:30 +0200

    build: album controller with roles

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -2,14 +2,39 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Album;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class AlbumController extends Controller
 {
+
+    public function __construct()
+    {
+        $response = $this->middleware('can:view, ' . Album::class)->only('view');
+        if ($response) {
+            // The action is authorized...
+        } else {
+            echo $response->message();
+        }
+    }
+
     public function show()
     {
         $album = Album::all()->toArray();
         return response()->json(["data" => $album, "success" => true, "type" => 'success']);
+    }
+
+    public function view(Request $request, int|string $params)
+    {
+
+        $role = $request->user()->role();
+        $user = $request->user();
+        $album = Album::all();
+
+        $gallery = Album::where('folder', $params)->orWhere('id', $params)->first()->pictures->toArray();
+
+
+        return response()->json(["data" => $gallery, "success" => true, "type" => "success"]);
     }
 }
