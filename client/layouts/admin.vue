c04ee620e9403d415383ef8cd8609b808ad8ad5f
Nicolas Chevalier <nicolas.mailip@gmail.com>
2022-07-18 19:29:47 +0200

    fix: only client, remove SSR on dashboard

--- client/layouts/admin.vue
+++ client/layouts/admin.vue
@@ -1,24 +1,33 @@
-<template>
-    <div class="bg-oceanBackground absolute -z-1 backdrop-blur-md h-full w-full bg-center bg-no-repeat bg-cover">
-        <div class="p-12">
-            <div
-                class="flex h-full drop-shadow-container fixedfont-sans flex-row max-h-[85vh] bg-[#F2F4F5] rounded-3xl overflow-hidden">
-                <!-- Side bar-->
-                <div
-                    class="mobile:hidden flex h-[85vh] text-lg w-[128px] bg-[#1D2558] rounded-tl-3xl rounded-bl-[20px]">
-                    <div class="h-32 bg-[#1D2558] rounded-tl-3xl">
+<script setup>
 
+</script>
+<template>
+    <div>
+        <client-only>
+            <div
+                class="bg-oceanBackground absolute -z-1 backdrop-blur-md h-full w-full bg-center bg-no-repeat bg-cover">
+                <div class="p-12">
+                    <div
+                        class="flex h-full drop-shadow-container fixedfont-sans flex-row max-h-[90vh] bg-[#F2F4F5] rounded-3xl overflow-hidden">
+                        <!-- Side bar-->
+                        <div
+                            class="mobile:hidden flex h-[90vh] text-lg w-[128px] bg-[#1D2558] rounded-tl-3xl rounded-bl-[20px]">
+                            <div class="h-32 bg-[#1D2558] rounded-tl-3xl">
+
+                            </div>
+                            <nav>
+                                <ul class="flex flex-col justify-content-center"></ul>
+                            </nav>
+                        </div>
+                        <!-- END SIDEBAR -->
+                        <slot></slot>
                     </div>
-                    <nav>
-                        <ul class="flex flex-col justify-content-center"></ul>
-                    </nav>
                 </div>
-                <!-- END SIDEBAR -->
-                <slot></slot>
             </div>
-        </div>
+        </client-only>
     </div>
 </template>
+
 <style scoped>
 body {
     overflow: hidden;
