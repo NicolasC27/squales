3712e6e5580705cbc28b70e234db978e671f8530
Nicolas Chevalier <nicolas.mailip@gmail.com>
2022-07-19 00:19:51 +0200

    refactor: script and template style

--- client/pages/admin/dashboard/index.vue
+++ client/pages/admin/dashboard/index.vue
@@ -1,26 +1,35 @@
 <script setup lang="ts">
 import { OnClickOutside, vOnClickOutside } from '@vueuse/components'
-import { authStore } from "~/store/auth";
-
 import { onClickOutside } from '@vueuse/core'
+
+/*
+** Define interface
+*/
 export interface Gallery {
     name: string
 }
+
+/*
+** Define page meta
+*/
 definePageMeta({
-    layout: 'admin'
+    layout: 'admin',
+    middleware: 'authenticated',
 })
 
+/*
+** Define variable
+*/
 const { $api } = useNuxtApp()
 const galleries = ref<Gallery[]>()
 const emit = defineEmits(["data"]);
-const modal = ref(false)
-
-
-const get = async () => galleries.value = (await $api.get<Gallery[]>('/api/admin/gallery')).data
 
 const showModal = ref(false)
 const showModalCreate = ref(false)
 const data = ref(undefined)
+const target = ref(null)
+const targetAlbum = ref(null)
+
 const obj = reactive({ albumcreated: false })
 
 watch(
@@ -30,84 +39,76 @@ watch(
     }
 )
 
+const getGallery = async () => galleries.value = (await $api.get<Gallery[]>('/api/admin/gallery')).data
 
-function openModal(gallery) {
+
+/*
+** Define function
+*/
+function openModal(gallery): void {
     this.data = gallery;
-    console.log('open: ' + showModal.value)
     showModal.value = !showModal.value;
-    console.log('open after : ' + showModal.value)
-
 }
 
-function openModalCreateAlbum() {
+function openModalCreateAlbum(): void {
     showModalCreate.value = !showModalCreate.value;
 }
 
-const target = ref(null)
-const targetAlbum = ref(null)
+function checkCreation(check): void {
+    obj.albumcreated = check;
+    if (obj.albumcreated)
+        getGallery();
+}
 
-onClickOutside(target, (event) => {
+onClickOutside(target, (event): void => {
     showModal.value = false;
 })
 
-onClickOutside(targetAlbum, (event) => {
+onClickOutside(targetAlbum, (event): void => {
     showModalCreate.value = false;
 })
 
-function checkCreation(check) {
-    obj.albumcreated = check;
-    console.log(obj.albumcreated);
-}
-
-get();
-
-
-const auth = authStore();
-
-auth.proceedSignIn({ email: 'thieery.fewf@gmail.com', password: '23111996' });
+getGallery();
 </script>
 <template>
-    <div class="h-full w-full h-[85vh] relative">
-        <button type="button">
-            {{ auth.token }}
-        </button>
+    <div class="flex flex-col h-[90vh] w-full relative">
         <div>
             <div
-                class="flex self-center justify-center h-16 w-80 bg-admin-sidebarTop m-auto rounded-br-2xl rounded-bl-2xl">
-                <p class="self-center text-white font-medium text-center">{{ $route.name }}</p>
+                class="bg-admin-sidebarTop flex m-auto rounded-br-2xl rounded-bl-2xl h-16 w-80 self-center justify-center">
+                <p class="font-medium text-white text-center self-center">{{ $route.name }}</p>
             </div>
         </div>
-        <div class="pt-32 flex flex-row flex-start gap-24 justify-center">
-            <div class="relative w-528 h-56 bg-admingalleryAddAlbum bg-cover rounded-2xl">
+        <div class="flex flex-row flex-start pt-32 gap-24 justify-center">
+            <div class="bg-admingalleryAddAlbum bg-cover rounded-2xl h-56 w-528 relative">
                 <button @click="openModalCreateAlbum()"
-                    class="absolute bottom-6 right-6 w-44 h-11 bg-admin-white rounded-lg">
+                    class="bg-admin-white rounded-lg h-11 right-6 bottom-6 w-44 absolute">
                     <p class="font-medium">Créer un album</p>
                 </button>
             </div>
-            <div class="relative w-528 h-56 bg-admingalleryAddAlbum bg-cover rounded-2xl">
-                <button class="absolute bottom-6 right-6 w-44 h-11 bg-admin-white rounded-lg">
+            <div class="bg-admingalleryAddAlbum bg-cover rounded-2xl h-56 w-528 relative">
+                <button class="bg-admin-white rounded-lg h-11 right-6 bottom-6 w-44 absolute">
                     <p class="font-medium">Créer un album</p>
                 </button>
             </div>
         </div>
-        <div class="pt-12">
-            <div class="w-3/4 h-1 bg-admin-gutter m-auto rounded-2xl"></div>
+        <div class="pt-12 pb-12">
+            <div class="m-auto bg-yellow-300 rounded-2xl h-1 w-3/4"></div>
         </div>
-        <div class="flex m-auto justify-center">
-            <div v-for="(gallery, index) in galleries" :key="index">
-                <div class="relative w-px-115 h-56 bg-admingalleryExample bg-cover rounded-2xl">
+        <div class="flex flex-row flex-wrap m-auto w-4/5 gallery justify-center overflow-auto">
+            <div v-for="(gallery, index) in galleries" :key="index" class="p-3">
+                <div class="bg-admingalleryExample bg-cover rounded-2xl h-56 w-[460px] relative ">
                     <button @click="openModal(gallery)"
-                        class="absolute bottom-6 right-6 w-44 h-11 bg-admin-white rounded-lg">
+                        class="bg-admin-white rounded-lg h-11 right-6 bottom-6 w-44 absolute ">
                         <p class="font-medium">{{ gallery.name }}</p>
                     </button>
                 </div>
             </div>
         </div>
         <GalleryModal
-            class="slidein bg-admin-white h-full rounded-tr-3xl rounded-br-3xl p-8 w-2/5 slidein absolute overflow-auto border-l-5 border-yellow-400"
+            class="bg-admin-white h-full border-l-[7px] border-yellow-300 rounded-tr-3xl rounded-br-3xl p-8 w-[42%] slidein absolute overflow-auto "
             v-show="showModal" :data="data" ref="target"></GalleryModal>
         <AlbumModal
-            class="slidein bg-admin-white h-full rounded-tr-3xl rounded-br-3xl p-8 w-2/5 slidein absolute overflow-auto border-l-5 border-yellow-400"
+            class="bg-admin-white h-full border-l-[7px] border-yellow-400 rounded-tr-3xl rounded-br-3xl p-8  w-[42%] slidein absolute overflow-auto "
             @albumcreated="checkCreation" v-show="showModalCreate" ref="targetAlbum"></AlbumModal>
     </div>
 </template>
@@ -141,5 +142,9 @@ auth.proceedSignIn({ email: 'thieery.fewf@gmail.com', password: '23111996' });
 
 .slidein::-webkit-scrollbar-corner {
     background-color: transparent;
+}
+
+.gallery::-webkit-scrollbar {
+    width: 0 !important
 }
 </style>
