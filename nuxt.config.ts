3efc2f09c1383640c7b9654f90b2f91a9990a089
Nicolas Chevalier <nicolas.mailip@gmail.com>
2022-07-18 19:45:56 +0200

    fix: issue npm run build
    delete flowbite plugin and fix nuxt config tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,3 @@
-import { plugin } from "windicss/plugin";
 import { defineNuxtConfig } from "nuxt3";
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
@@ -17,16 +16,17 @@ export default defineNuxtConfig({
     },
   },
 
-  css: ["~/assets/scss/index.scss"],
+  css: ["~/assets/scss/index.scss", "~/assets/css/tailwind.css"],
 
   components: {
     dirs: ["@/components"],
     global: true,
   },
 
+  modules: ["@nuxtjs/tailwindcss"],
+
   plugin: [
     "~/plugins/api",
-    "~/plugins/flowbite.client",
     "~/plugins/lazy-loading",
     "~/plugins/persistedstate",
     "~/plugins/services",
@@ -38,8 +38,6 @@ export default defineNuxtConfig({
     ["@pinia/nuxt", { disableVuex: false }],
   ],
   devServerHandlers: [],
-
-  modules: ["@nuxtjs/tailwindcss"],
 
   publicRuntimeConfig: {
     apiURL: process.env.API_URL || "http://localhost:80/",
