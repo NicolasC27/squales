2fc531b772e4db3351e7d3af60f4edcf63944522
Nicolas Chevalier <nicolas.mailip@gmail.com>
2022-07-18 19:48:39 +0200

    chore: config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,74 +1,75 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
+  darkMode: "class",
   content: [
     "./node_modules/flowbite/**/*.js",
-    'components/**/*.vue',
-    'layouts/**/*.vue',
-    'pages/**/*.vue',
-    'plugins/**/*.js',
-    'content/**/*.md',
+    "components/**/*.vue",
+    "layouts/**/*.vue",
+    "pages/**/*.vue",
+    "plugins/**/*.js",
+    "content/**/*.md",
   ],
   theme: {
     boxShadow: {
-      'datatable': '0 1px 3px 0 rgb(0 0 0 / 10%), 0 1px 2px 0 rgb(0 0 0 / 6%)',
+      datatable: "0 1px 3px 0 rgb(0 0 0 / 10%), 0 1px 2px 0 rgb(0 0 0 / 6%)",
     },
     dropShadow: {
-      'container': '0 0 69px 6px rgba(0, 0, 0, 0.15)',
-      'datatable': '0 1px 3px 0 rgb(0 0 0 / 10%), 0 1px 2px 0 rgb(0 0 0 / 6%)',
+      container: "0 0 69px 6px rgba(0, 0, 0, 0.15)",
+      datatable: "0 1px 3px 0 rgb(0 0 0 / 10%), 0 1px 2px 0 rgb(0 0 0 / 6%)",
     },
-    backgroundColor: theme => ({
-      ...theme('colors'),
+    backgroundColor: (theme) => ({
+      ...theme("colors"),
       test: {
-        light: '#55b3d1',
-        yellow: '#FFD600',
+        light: "#55b3d1",
+        yellow: "#FFD600",
       },
       admin: {
-        sidebar: '#17277D',
-        sidebarTop: '#1D2558',
-        background: '#F2F4F5',
-        white: '#FFFFFF',
-        gutter: '#E5E5E5',
-        button: '#C4C4C4',
+        sidebar: "#17277D",
+        sidebarTop: "#1D2558",
+        background: "#F2F4F5",
+        white: "#FFFFFF",
+        gutter: "#E5E5E5",
+        button: "#C4C4C4",
       },
     }),
     backgroundPosition: {
-      'center': 'center',
+      center: "center",
     },
     extend: {
       backgroundImage: {
-        'oceanBackground': 'url("/images/admin/Ocean-Background.jpg")',
-        'dive-banner': 'url("/images/front/banner.jpeg")',
-        'footer': 'url("/images/front/footer.png")',
-        'gallery': 'url("/images/front/banner/gallery.jpeg")',
-        'admingalleryAddAlbum': 'url("/images/admin/gallery-new-album.jpg")',
-        'admingalleryExample': 'url("/images/admin/image_example.jpg")',
+        oceanBackground: 'url("/images/admin/Ocean-Background.jpg")',
+        "dive-banner": 'url("/images/front/banner.jpeg")',
+        footer: 'url("/images/front/footer.png")',
+        gallery: 'url("/images/front/banner/gallery.jpeg")',
+        admingalleryAddAlbum: 'url("/images/admin/gallery-new-album.jpg")',
+        admingalleryExample: 'url("/images/admin/image_example.jpg")',
       },
       width: {
-        '528': '33rem',
-      }
+        528: "33rem",
+      },
+      cursor: {
+        deleteMode:
+          "url(\"data:image/svg+xml;charset=UTF-8,%3csvg width='32' height='32' fill='none' stroke='white' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'%3e%3cpath stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z'%3e%3c/path%3e%3c/svg%3e\"), pointer",
+      },
     },
-    fontFamily: {
-      'sans': ['Quicksand', 'Arial', 'sans-serif'],
-      'title-homepage-h1': ['Rock Salt', 'Arial', 'sans-serif'],
-    },
-    textColor: theme => ({
-      ...theme('colors'),
-      'default': '#6A6A6A',
+
+    textColor: (theme) => ({
+      ...theme("colors"),
+      default: "#6A6A6A",
       white: "#FFF",
-      grey: '#C3C6CC',
+      grey: "#C3C6CC",
     }),
     screens: {
-      'mobile': { 'max': '639px' },
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
-    }
+      mobile: { max: "639px" },
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
   },
   plugins: [
-    require('flowbite/plugin'),
+    require("flowbite/plugin"),
     // plugin(({ addComponents }) => {
     //   const buttons = {
     //     '.btn': {
@@ -99,4 +100,4 @@ module.exports = {
     //   addComponents(buttons)
     // }),
   ],
-}
+};
