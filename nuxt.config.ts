3242e1b0f2fbc60aec7cb61bd3e71abf66bfed9d
Nicolas Chevalier <nicolas.mailip@gmail.com>
2022-08-28 14:08:57 +0200

    chore: global app information and flowbite proccess

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,24 @@
-import { defineNuxtConfig } from "nuxt3";
+import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   srcDir: "client/",
+
+  app: {
+    head: {
+      title: "SQUALES",
+      meta: [
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          hid: "description",
+          name: "description",
+          content: "Nuxt 3 Awesome Starter",
+        },
+      ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
+  },
+
   build: {
     extends(config: any, ctx: any, { isDev, isClient, loaders: { vue } }) {
       if (ctx.isDev) {
@@ -15,6 +31,7 @@ export default defineNuxtConfig({
       }
     },
   },
+  pageTransition: { "page-fade": true },
 
   css: ["~/assets/scss/index.scss", "~/assets/css/tailwind.css"],
 
@@ -30,6 +47,7 @@ export default defineNuxtConfig({
     "~/plugins/lazy-loading",
     "~/plugins/persistedstate",
     "~/plugins/services",
+    "@/plugins/flowbite.client.ts",
   ],
 
   buildModules: [
@@ -38,6 +56,11 @@ export default defineNuxtConfig({
     ["@pinia/nuxt", { disableVuex: false }],
   ],
   devServerHandlers: [],
+
+  // experimental features
+  experimental: {
+    reactivityTransform: true,
+  },
 
   publicRuntimeConfig: {
     apiURL: process.env.API_URL || "http://localhost:80/",
